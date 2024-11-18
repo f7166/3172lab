e429eca8a5df62014601175d23171c003370d273
lin2 <zh469010@dal.ca>
2024-11-18 16:51:47 -0400

    test

---
+++ lab7/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {picture} from './data/picture';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>{picture.title}</h1>
+                <img src={picture.imageUrl} className="pictureName"/>
+                <p>{picture.description}</p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
---
+++ lab7/src/data/picture.js
@@ -0,0 +1,5 @@
+export const picture = {
+    title: "Cat Picture",
+    imageUrl: "https://www.alleycat.org/wp-content/uploads/2019/03/FELV-cat.jpg",
+    description: "This is a cat picture"
+};
